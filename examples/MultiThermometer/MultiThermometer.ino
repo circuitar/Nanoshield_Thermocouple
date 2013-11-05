b6f9cdf6f2578f7a15d8ad79bbb343aa158a9913
Luis Fernando Chavier <lfchavier@circuitar.com.br>
2013-11-04 22:57:30 -0200

    Added example using multiple boards

---
+++ examples/MultiThermometer/MultiThermometer.ino
@@ -0,0 +1,61 @@
+/*
+Read temperatures from multiple Thermocouple Nanoshields.
+ 
+ Copyright (c) 2013 Circuitar
+ This software is released under the MIT license. See the attached LICENSE file for details.
+ */
+#include <SPI.h>
+#include "Nanoshield_Thermocouple.h"
+
+// Use 6 thermocouples simultaneously
+#define THERMOCOUPLES 6
+
+// Create an array of thermocouples
+Nanoshield_Thermocouple thermocouple[THERMOCOUPLES];
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("------------------");
+  Serial.println(" MultiThermometer");
+  Serial.println("------------------");
+  Serial.println("");
+
+  // Initialize each Thermocouple Nanoshield, selecting the corresponding CS pin
+  for(int i = 0; i < THERMOCOUPLES; i++) {
+    thermocouple[i].begin(i + 5);
+  }
+}
+
+void loop()
+{
+  for(int i = 0; i < THERMOCOUPLES; i++) {
+    // Read thermocouple data
+    thermocouple[i].read();
+    
+    // Print thermocouple number
+    Serial.print(i + 1);
+    Serial.print(": ");
+  
+    // Print temperature in the serial port, checking for errors
+    if (thermocouple[i].isShortedToVcc()) {
+      Serial.println("Shorted to VCC");
+    } 
+    else if (thermocouple[i].isShortedToGnd()) {
+      Serial.println("Shorted to GND");
+    } 
+    else if (thermocouple[i].isOpen()) {
+      Serial.println("Open circuit");
+    } 
+    else {
+      Serial.print(thermocouple[i].getExternal());
+      Serial.print(", ");
+      Serial.println(thermocouple[i].getInternal());
+    }
+  }
+
+  // Wait for next second
+  delay(1000);
+}
+
+
